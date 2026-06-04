deb5ef3eb794652c7bc29229acc97222a14c8958
DaisukeUe <naruhodosan012@gmail.com>
2026-06-04 09:18:05 +0900

    発言取得

--- back/server.js
+++ back/server.js
@@ -2,9 +2,19 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { getTrump } = require("./getTruth");
+const { translate } = require("@vitalets/google-translate-api");
+const { createResponseBody } = require("./view/views");
+
+const { responseTrump } = createResponseBody();
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.use(express.json());
+
+app.get("/toranpu", (res, req) => {
+  responseTrump(req, res);
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
