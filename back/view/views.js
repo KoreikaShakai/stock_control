7ce8865f38e75e20d28dea71534e48673e7306a7
DaisukeUe <naruhodosan012@gmail.com>
2026-06-04 18:11:13 +0900

    アップデート、デレート

--- back/view/views.js
+++ back/view/views.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { resolve } = require("dns");
 const createResponseBody = () => {
-  const responseTrump = async (res, req) => {
+  const responseTrump = async (req, res) => {
     console.log("アクセス");
     let result = [];
     try {
@@ -26,28 +26,9 @@ const createResponseBody = () => {
         } else {
           post.content_ja = "(本文なし)";
         }
-        return post;
+        console.log("データ取得完了");
+        translatedData.push(post);
       }
-
-      //   const translatedData = await Promise.all(
-      //     data.map(async (post) => {
-      //       if (post.content && post.content.trim() !== "") {
-      //         try {
-      //           const resTranslate = await translate(post.content, {
-      //             from: "en",
-      //             to: "ja",
-      //           });
-      //           post.content_ja = resTranslate.text;
-      //         } catch (transError) {
-      //           console.error("翻訳エラー:", transError.message);
-      //           post.content_ja = "(翻訳失敗)";
-      //         }
-      //       } else {
-      //         post.content_ja = "(本文なし)";
-      //       }
-      //       return post;
-      //     }),
-      //   );
       await translatedData.forEach((post) => {
         result.push({
           post_date: post.account.last_status_at,
