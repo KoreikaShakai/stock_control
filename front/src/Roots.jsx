02518ab2160ec8057504bfbc002a2a6734f171b4
simo-rade <simo@mail.toyota.co.jp>
2026-06-05 15:55:54 +0900

    tuika

--- front/src/Roots.jsx
+++ front/src/Roots.jsx
@@ -1,12 +1,14 @@
 import App from "./App";
 import { Routes, Route } from "react-router";
 import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
 
 function Roots() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/main" element={<App />} />
       </Routes>
     </>
